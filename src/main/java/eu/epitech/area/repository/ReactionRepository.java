d2c17b3682cb5308e4e085d2ef8b8450f2339aab
Guillaume MARDON <contact@gmardon.com>
2018-01-02 15:22:27 +0100

    fix reaction repository bug

--- src/main/java/eu/epitech/area/repository/ReactionRepository.java
+++ src/main/java/eu/epitech/area/repository/ReactionRepository.java
@@ -1,6 +1,7 @@
 package eu.epitech.area.repository;
 
+import eu.epitech.area.reaction.Reaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReactionRepository extends JpaRepository<ReactionRepository, Long> {
+public interface ReactionRepository extends JpaRepository<Reaction, Long> {
 }
